4b3445f0bfe67368f3f41dadfa8d17a21e3533be
ViktoriaSharova <131592560+ViktoriaSharova@users.noreply.github.com>
2024-01-19 18:53:18 +0100

    first commit

---
+++ contacts.js
@@ -0,0 +1,66 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+async function listContacts() {
+  try {
+    const list = await fs.readFile(contactsPath, 'utf-8');
+    const contacts = JSON.parse(list);
+    return contacts;
+  } catch (error) {
+    console.error("Error reading contacts:", error.message);
+    throw error;
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    const result = contacts.find((item) => item.id === contactId);
+    return result || null;
+  } catch (error) {
+    console.error("Error reading contacts:", error.message);
+    throw error;
+  }
+}
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    console.error("Error reading contacts:", error.message);
+    throw error;
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((item) => item.id === contactId);
+    if (index === -1) return null;
+
+    const [result] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return result;
+  } catch (error) {
+    console.error("Error reading contacts:", error.message);
+    throw error;
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
---
+++ index.js
@@ -0,0 +1,51 @@
+const { program } = require("commander");
+const contacts = require("./contacts");
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allList = await contacts.listContacts();
+      console.log(allList);
+      break;
+
+    case "get":
+      const contactById = await contacts.getContactById(id);
+      console.log(contactById);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      console.log(removeContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+// invokeAction({ action: "list" });
+// invokeAction({ action: "get", id: "qdggE76Jtbfd9eWJHrssH" });
+// invokeAction({
+//   action: "add",
+//   name: "Chaim Lewis",
+//   email: "dui.in@egetlacus.ca",
+//   phone: "(294) 840-6685",
+// });
+// invokeAction({ action: "remove", id: "qdggE76Jtbfd9eWJHrssH" });
+invokeAction(options);
